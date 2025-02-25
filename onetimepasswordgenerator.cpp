f02d01217fc8c00ebb32eb01868de92705afa076
Dominik Chrastecky <dominik@chrastecky.cz>
2025-02-25 18:38:40 +0100

    Add optional algorithm parameter

--- onetimepasswordgenerator.cpp
+++ onetimepasswordgenerator.cpp
@@ -4,6 +4,7 @@
 #include <QtEndian>
 #include <QDateTime>
 #include <QtMath>
+#include <QDebug>
 
 OneTimePasswordGenerator::OneTimePasswordGenerator(QObject *parent)
     : QObject{parent}
@@ -11,44 +12,44 @@ OneTimePasswordGenerator::OneTimePasswordGenerator(QObject *parent)
 
 }
 
-QByteArray OneTimePasswordGenerator::generateHOTP(const QByteArray &rawSecret, quint64 counter, int length)
+QByteArray OneTimePasswordGenerator::generateHOTP(const QByteArray &rawSecret, quint64 counter, int length, const QCryptographicHash::Algorithm hash)
 {
 #if Q_BYTE_ORDER == Q_LITTLE_ENDIAN
-        counter = qToBigEndian(counter);
+    counter = qToBigEndian(counter);
 #endif
-        QByteArray data;
-        data.reserve(8);
-        for (int i = 7; i >= 0; --i) {
-            data.append(counter & 0xff);
-            counter >>= 8;
-        }
-        QMessageAuthenticationCode mac(QCryptographicHash::Sha1);
-        mac.setKey(rawSecret);
-        mac.addData(data);
-        QByteArray hmac = mac.result();
-        int offset = hmac.at(hmac.length() - 1) & 0xf;
-        quint32 truncatedHash = ((hmac.at(offset) & 0x7f) << 24)
-                                | ((hmac.at(offset + 1) & 0xff) << 16)
-                                | ((hmac.at(offset + 2) & 0xff) << 8)
-                                | (hmac.at(offset + 3) & 0xff);
-        int modulus = int(qPow(10, length));
-        return QByteArray::number(truncatedHash % modulus, 10).rightJustified(length, '0');
+    QByteArray data;
+    data.reserve(8);
+    for (int i = 7; i >= 0; --i) {
+        data.append(counter & 0xff);
+        counter >>= 8;
+    }
+    QMessageAuthenticationCode mac(hash);
+    mac.setKey(rawSecret);
+    mac.addData(data);
+    QByteArray hmac = mac.result();
+    int offset = hmac.at(hmac.length() - 1) & 0xf;
+    quint32 truncatedHash = ((hmac.at(offset) & 0x7f) << 24)
+                            | ((hmac.at(offset + 1) & 0xff) << 16)
+                            | ((hmac.at(offset + 2) & 0xff) << 8)
+                            | (hmac.at(offset + 3) & 0xff);
+    int modulus = int(qPow(10, length));
+    return QByteArray::number(truncatedHash % modulus, 10).rightJustified(length, '0');
 }
 
-QString OneTimePasswordGenerator::generateHOTP(const QString &secret, quint64 counter, int length)
+QString OneTimePasswordGenerator::generateHOTP(const QString &secret, quint64 counter, int length, const QString &hash)
 {
-    return generateHOTP(fromBase32(secret), counter, length);
+    return generateHOTP(fromBase32(secret), counter, length, convertAlgorithm(hash));
 }
 
-QByteArray OneTimePasswordGenerator::generateTOTP(const QByteArray &rawSecret, int length)
+QByteArray OneTimePasswordGenerator::generateTOTP(const QByteArray &rawSecret, int length, const QCryptographicHash::Algorithm hash)
 {
     const qint64 counter = QDateTime::currentDateTime().toMSecsSinceEpoch() / 30000;
-    return generateHOTP(rawSecret, counter, length);
+    return generateHOTP(rawSecret, counter, length, hash);
 }
 
-QString OneTimePasswordGenerator::generateTOTP(const QString &secret, int length)
+QString OneTimePasswordGenerator::generateTOTP(const QString &secret, int length, const QString &hash)
 {
-    return generateTOTP(fromBase32(secret), length);
+    return generateTOTP(fromBase32(secret), length, convertAlgorithm(hash));
 }
 
 QByteArray OneTimePasswordGenerator::fromBase32(const QString &input)
@@ -74,4 +75,21 @@ QByteArray OneTimePasswordGenerator::fromBase32(const QString &input)
         }
     }
     return result;
+}
+
+QCryptographicHash::Algorithm OneTimePasswordGenerator::convertAlgorithm(const QString &algorithmName)
+{
+    const auto normalizedName = algorithmName.toUpper();
+
+    if (normalizedName == "SHA1") {
+        return QCryptographicHash::Sha1;
+    } else if (normalizedName == "SHA256") {
+        return QCryptographicHash::Sha256;
+    } else if (normalizedName == "SHA512") {
+        return QCryptographicHash::Sha512;
+    }
+
+    qWarning() << "Invalid hash algorithm " << algorithmName << ", defaulting to Sha1";
+
+    return QCryptographicHash::Sha1;
 }
--- onetimepasswordgenerator.h
+++ onetimepasswordgenerator.h
@@ -3,19 +3,22 @@
 
 #include <QObject>
 
+#include <QCryptographicHash>
+
 class OneTimePasswordGenerator : public QObject
 {
     Q_OBJECT
 public:
     explicit OneTimePasswordGenerator(QObject *parent = nullptr);
 
-    QByteArray generateHOTP(const QByteArray &rawSecret, quint64 counter, int length);
-    Q_INVOKABLE QString generateHOTP(const QString &secret, quint64 counter, int length);
+    QByteArray generateHOTP(const QByteArray &rawSecret, quint64 counter, int length, const QCryptographicHash::Algorithm hash = QCryptographicHash::Sha1);
+    Q_INVOKABLE QString generateHOTP(const QString &secret, quint64 counter, int length, const QString &hash = "SHA1");
 
-    QByteArray generateTOTP(const QByteArray &rawSecret, int length);
-    Q_INVOKABLE QString generateTOTP(const QString &secret, int length);
+    QByteArray generateTOTP(const QByteArray &rawSecret, int length, const QCryptographicHash::Algorithm hash = QCryptographicHash::Sha1);
+    Q_INVOKABLE QString generateTOTP(const QString &secret, int length, const QString &hash = "SHA1");
 private:
     QByteArray fromBase32(const QString &input);
+    QCryptographicHash::Algorithm convertAlgorithm(const QString &algorithmName);
 
 signals:
 
